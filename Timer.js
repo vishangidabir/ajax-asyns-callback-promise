6754ef8e4a102b963c0d9d2adb1fb9689229dcb1
vishangi <vishangid@gmail.com>
2022-08-15 22:27:31 +0530

    Welcome to ajax server calls

---
+++ Timer.js
@@ -0,0 +1,11 @@
+function showtime() {
+    const date = new Date();
+    return date.getHours() + "Hrs:" + date.getMinutes() + "Mins:" +date.getSeconds()+ "Secs";
+}
+function showSessionExpire() {
+    console.log("Activity-B: Your session expired at "+showtime());
+}
+
+console.log("Activity-A: Triggering Activity-B at "+showtime());
+setTimeout(showSessionExpire, 5000);
+console.log("Activity-A: Triggered Activity-B at "+showtime()+" will execute after 5 seconds");
